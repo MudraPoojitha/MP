5c87923325474a195d912b87ac02d8987ce15332
MudraPoojitha <122143254+MudraPoojitha@users.noreply.github.com>
2023-03-05 23:43:15 +0530

    Update BookingService.java

--- src/main/java/com/capg/service/BookingService.java
+++ src/main/java/com/capg/service/BookingService.java
@@ -11,9 +11,10 @@ import com.capg.exception.BookingNotFoundException;
 public interface BookingService {
 	
 	public String addBooking(Booking booking);
-	public List<Booking> listAllBookingsByUserId(String uemail_Id);
+	public List<Booking> listAllBookingsByuemail_Id(String uemail_Id);
 	public List<Booking> listAllBookingsInDateRange(LocalDate startDate,LocalDate endDate) throws BookingNotFoundException;
 	public Booking getBookingbyId(int bookingId) throws BookingNotFoundException;
-	public Booking deleteBooking(int bookingId) throws BookingNotFoundException;
-
+	public String deleteBooking(int bookingId) throws BookingNotFoundException;
+        public Booking updateBooking(Booking booking)throws BookingNotFoundException;
+	
 }
