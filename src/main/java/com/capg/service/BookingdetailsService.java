8bab15c164675e239a227afde9a2df59a8b46d77
MudraPoojitha <122143254+MudraPoojitha@users.noreply.github.com>
2023-03-05 23:37:45 +0530

    Update BookingdetailsService.java

--- src/main/java/com/capg/service/BookingdetailsService.java
+++ src/main/java/com/capg/service/BookingdetailsService.java
@@ -1,13 +1,13 @@
-package com.capg.service;
+// package com.capg.service;
 
-import java.util.List;
+// import java.util.List;
 
-import com.capg.entity.Bookingdetails;
-import com.capg.exception.BookingNotFoundException;
-public interface BookingdetailsService {
+// import com.capg.entity.Bookingdetails;
+// import com.capg.exception.BookingNotFoundException;
+// public interface BookingdetailsService {
 	
-	public Bookingdetails addBookingdetails(Bookingdetails bookingDetails);
-	public Bookingdetails updateBookingDetails(Bookingdetails bookingDetails)throws BookingNotFoundException;
-	public List<Bookingdetails> getAllBookingdetailsByBookingId(int bookingId);
+// 	public Bookingdetails addBookingdetails(Bookingdetails bookingDetails);
+// 	public Bookingdetails updateBookingDetails(Bookingdetails bookingDetails)throws BookingNotFoundException;
+// 	public List<Bookingdetails> getAllBookingdetailsByBookingId(int bookingId);
 
-}
+// }
