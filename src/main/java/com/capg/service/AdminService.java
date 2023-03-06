e3ee4e353cc9028fc15d6e466856c12ebaee6374
Harshini-Shanagaram <122144568+Harshini-Shanagaram@users.noreply.github.com>
2023-03-06 11:56:31 +0530

    Update AdminService.java

--- src/main/java/com/capg/service/AdminService.java
+++ src/main/java/com/capg/service/AdminService.java
@@ -1,8 +1,6 @@
 package com.capg.service;
 
-import org.springframework.stereotype.Service;
 import com.capg.entity.Admin;
-import com.capg.entity.Bookingdetails;
 import com.capg.exception.UserNotFoundException;
 
 
@@ -12,12 +10,13 @@ public interface AdminService {
 	public String loginAdmin(String emailId,String password)throws UserNotFoundException;
 	public Admin getAdminByEmailId(String aemailId)throws UserNotFoundException;
 	public Admin updateAdmin(Admin adminDetails)throws UserNotFoundException;
+	public String isAdmin(String emailId);
 	public Admin deleteAdmin(int adminId) throws UserNotFoundException;
 	public Admin findByAdminName(String adminName) throws UserNotFoundException;
-	public boolean grantAdminRights(String emailid)  throws UserNotFoundException;
-	public Bookingdetails approveBooking(int bookingId)throws UserNotFoundException;
-	public Bookingdetails rejectBooking(int bookingId)throws UserNotFoundException;
-	public Bookingdetails disallowBooking(int bookingId)throws UserNotFoundException;
-	public Bookingdetails approveCancellation(int bookingId)throws UserNotFoundException;
+	public String approveBooking(int bookingId)throws UserNotFoundException;
+	public String rejectBooking(int bookingId)throws UserNotFoundException;
+	public String disallowBooking(int bookingId)throws UserNotFoundException;
+	public String approveCancellation(int bookingId)throws UserNotFoundException;
+
 
 }
